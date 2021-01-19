e87ae678dda0f078475fe04a618e582a9f745e5a
Nico <nfoster@glamit.com.ar>
2021-01-19 02:02:18 -0300

    se agrega reproductor de video para tiempos verbales esenciales

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,24 @@ const videoContainer = document.querySelector("#video-container");
 const video = document.querySelector(".video");
 const close = document.querySelector(".close");
 
-function ShowVideo(links) {
+const _videoContainer = document.querySelector("#video-container2");
+const _video = document.querySelector(".video2");
+const _close = document.querySelector(".close2");
+
+function ShowSlider1(links) {
   videoContainer.style.display = "block";
   video.src = links;
 }
 
+function ShowSlider2(links) {
+  _videoContainer.style.display = "block";
+  _video.src = links;
+}
+
 close.onclick = () => {
   videoContainer.style.display = "none";
+};
+
+_close.onclick = () => {
+  _videoContainer.style.display = "none";
 };
